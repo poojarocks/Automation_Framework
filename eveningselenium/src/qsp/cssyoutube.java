7e6f3126162a63b665a01e9d21d7904a89def893
91849 <91849@DESKTOP-AIN97HJ>
2023-09-01 17:25:35 +0530

    Bulid B13

--- eveningselenium/src/qsp/cssyoutube.java
+++ eveningselenium/src/qsp/cssyoutube.java
@@ -9,9 +9,9 @@ public class cssyoutube {
 		System.setProperty("webdriver.chrome.driver", "./Softwares/chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.youtube.com");
-		Thread.sleep(2000);
-		driver.findElement(By.cssSelector("input[id='search']")).sendKeys("kgf trailer");
-		driver.findElement(By.cssSelector("button[id='search-icon-legacy']")).click();
+		//Thread.sleep(2000);
+		//driver.findElement(By.cssSelector("input[id='search']")).sendKeys("kgf trailer");
+		//driver.findElement(By.cssSelector("button[id='search-icon-legacy']")).click();
 		driver.findElement(By.cssSelector ("yt-formatted-string[class='style-scope ytd-video-renderer']")).click();
 
 
